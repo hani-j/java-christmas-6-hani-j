ad7b15b05399f9a9f92c4ee3de95d5571b9010e0
Haeun Jeong <gkdms4248@gmail.com>
2023-11-10 17:07:25 +0900

    test: d-day 할인 여부 확인 기능 테스트 추가

---
+++ src/test/java/christmas/ChristmasEventTest.java
@@ -0,0 +1,25 @@
+package christmas;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ChristmasEventTest {
+
+    @DisplayName("25 일 이하의 숫자가 들어오면 크리스마스 d-day 할인 여부가 true 이다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 10, 14, 25})
+    public void isDDayDiscount(int day) {
+        // given
+        ChristmasEvent christmasEvent = new ChristmasEvent();
+
+        // when
+        boolean isDDay = christmasEvent.isDDayDiscount(day);
+
+        // then
+        assertTrue(isDDay);
+    }
+}
