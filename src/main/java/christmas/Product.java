5e429143ef14a24d7fd57ff1b94e9b97600205c9
Haeun Jeong <gkdms4248@gmail.com>
2023-11-10 20:53:04 +0900

    feat: 메뉴의 이름이 같은지 확인 기능 추가

---
+++ src/main/java/christmas/Product.java
@@ -0,0 +1,23 @@
+package christmas;
+
+public class Product {
+    private final String name;
+    private final int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public boolean isSameName(String name) {
+        return this.name.equals(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
