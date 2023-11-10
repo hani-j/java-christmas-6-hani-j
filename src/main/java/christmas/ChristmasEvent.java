21761c5039c4f1ab211d69dc067bf6001ca2ae51
Haeun Jeong <gkdms4248@gmail.com>
2023-11-10 17:13:27 +0900

    feat: 평일 할인 여부 확인 기능 추가

--- src/main/java/christmas/ChristmasEvent.java
+++ src/main/java/christmas/ChristmasEvent.java
@@ -1,8 +1,15 @@
 package christmas;
 
+import static christmas.DayType.WEEKDAY;
+import static christmas.DayType.getDayType;
+
 public class ChristmasEvent {
 
     public boolean isDDayDiscount(int day) {
         return day <= 25;
+    }
+
+    public boolean isWeekdayDiscount(int day) {
+        return getDayType(day) == WEEKDAY;
     }
 }
