688886c2a94fb92ae9cedb0dcb7e2c2ce48cd125
Haeun Jeong <gkdms4248@gmail.com>
2023-11-10 17:24:53 +0900

    test: 주말 할인 여부 기능 추가

--- src/main/java/christmas/ChristmasEvent.java
+++ src/main/java/christmas/ChristmasEvent.java
@@ -1,6 +1,7 @@
 package christmas;
 
 import static christmas.DayType.WEEKDAY;
+import static christmas.DayType.WEEKEND;
 import static christmas.DayType.getDayType;
 
 public class ChristmasEvent {
@@ -11,5 +12,9 @@ public class ChristmasEvent {
 
     public boolean isWeekdayDiscount(int day) {
         return getDayType(day) == WEEKDAY;
+    }
+
+    public boolean isWeekendDiscount(int day) {
+        return getDayType(day) == WEEKEND;
     }
 }
