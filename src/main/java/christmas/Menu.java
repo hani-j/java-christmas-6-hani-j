cd07505ae142373d0f368a3d695d2d4f0ab94bb6
Haeun Jeong <gkdms4248@gmail.com>
2023-11-10 23:49:17 +0900

    refactor: 메뉴판 생성 enum 이용하는 것으로 수정

--- src/main/java/christmas/Menu.java
+++ src/main/java/christmas/Menu.java
@@ -1,10 +1,5 @@
 package christmas;
 
-import static christmas.Category.APPETIZER;
-import static christmas.Category.BEVERAGE;
-import static christmas.Category.DESSERT;
-import static christmas.Category.MAIN;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,34 +12,9 @@ public class Menu {
     }
 
     private void initMenu() {
-        initAppetizer();
-        initMain();
-        initDessert();
-        initBeverage();
-    }
-
-    private void initAppetizer() {
-        products.put("양송이수프", new Product(APPETIZER, 6000));
-        products.put("타파스", new Product(APPETIZER, 5500));
-        products.put("시저샐러드", new Product(APPETIZER, 8000));
-    }
-
-    private void initMain() {
-        products.put("티본스테이크", new Product(MAIN, 55_000));
-        products.put("바비큐립", new Product(MAIN, 54_000));
-        products.put("해산물파스타", new Product(MAIN, 35_000));
-        products.put("크리스마스파스타", new Product(MAIN, 25_000));
-    }
-
-    private void initDessert() {
-        products.put("초코케이크", new Product(DESSERT, 15_000));
-        products.put("아이스크림", new Product(DESSERT, 5_000));
-    }
-
-    private void initBeverage() {
-        products.put("제로콜라", new Product(BEVERAGE, 3_000));
-        products.put("레드와인", new Product(BEVERAGE, 60_000));
-        products.put("샴페인", new Product(BEVERAGE, 25_000));
+        MenuItem.getAllValues().stream().forEach(menuItem -> {
+            products.put(menuItem.getName(), new Product(menuItem.getCategory(), menuItem.getPrice()));
+        });
     }
 
     private Product getProduct(String name) {
