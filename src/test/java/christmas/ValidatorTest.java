436f1199e5279be50fe1c0f3463d0ebe5a5e3001
Haeun Jeong <gkdms4248@gmail.com>
2023-11-12 17:07:58 +0900

    test: 주문 형식이 다른 경우 검사 테스트 추가

--- src/test/java/christmas/ValidatorTest.java
+++ src/test/java/christmas/ValidatorTest.java
@@ -8,14 +8,13 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ValidatorTest {
-    
+
+    private Validator validator = new Validator();
+
     @DisplayName("1-31 의 숫자인 경우 throw 가 발생하지 않는다.")
     @ParameterizedTest
     @ValueSource(strings = {"1", "2", "3", "30", "31"})
     void validateDay(String day) {
-        // given
-        Validator validator = new Validator();
-
         // when & then
         assertDoesNotThrow(() -> validator.validateDay(day));
     }
@@ -24,12 +23,25 @@ public class ValidatorTest {
     @ParameterizedTest
     @ValueSource(strings = {"1.", ".2", "ㄱ", "one", "32", "44"})
     void validateNotDay(String day) {
-        // given
-        Validator validator = new Validator();
-
         // when & then
         assertThatThrownBy(() -> validator.validateDay(day))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
+    @DisplayName("메뉴 형식에 맞는 경우 Throw 가 발생하지 않는다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"해산물파스타-2,레드와인-1,초코케이크-1"})
+    void validateOrderFormat(String order) {
+        // when & then
+        assertDoesNotThrow(() -> validator.validateOrderFormat(order));
+    }
+
+    @DisplayName("메뉴 형식에 맞지 않는 경우 Throw 가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"해산물파스타-2, 레드와인-1,초코케이크-1", "해산물파스타2, 레드와인1,초코케이크1"})
+    void validateOrderFormat(String order) {
+        // when & then
+        assertThatThrownBy(() -> validator.validateOrderFormat(order));
     }
 }
