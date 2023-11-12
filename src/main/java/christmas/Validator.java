096e21cd550e431f24c10ba2964cff99e7a3a16e
Haeun Jeong <gkdms4248@gmail.com>
2023-11-12 16:40:55 +0900

    feat: 1-31 숫자 유효성 검사 추가

--- src/main/java/christmas/Validator.java
+++ src/main/java/christmas/Validator.java
@@ -6,7 +6,35 @@ public class Validator {
 
     private static final Pattern NUMBER = Pattern.compile("[0-9]+");
 
-    public boolean validateNumber(String number) {
-        return NUMBER.matcher(number).matches();
+    public void validateDay(String number) {
+        try {
+            validateNumber(number);
+            validateNumberSize(number);
+            validateInRange(number);
+        } catch (Exception exception) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void validateNumber(String number) {
+        if (!NUMBER.matcher(number).matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumberSize(final String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (Exception exception) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateInRange(String number) {
+        int day = Integer.parseInt(number);
+
+        if (day < 1 || day > 31) {
+            throw new IllegalArgumentException();
+        }
     }
 }
