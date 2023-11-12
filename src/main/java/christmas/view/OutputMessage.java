e6fe60079170a0ccef550f3727897a763383b441
Haeun Jeong <gkdms4248@gmail.com>
2023-11-12 19:46:38 +0900

    feat: 주문 메뉴 출력 기능 추가

--- src/main/java/christmas/view/OutputMessage.java
+++ src/main/java/christmas/view/OutputMessage.java
@@ -6,6 +6,7 @@ public enum OutputMessage {
     REQUEST_ORDER("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
     PREVIEW("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
     ORDER_MENU("<주문 메뉴>"),
+    AMOUNT("%s %d개\n"),
     TOTAL_AMOUNT("<할인 전 총주문 금액>"),
     GIVEAWAY_MENU("<증정 메뉴>"),
     CHAMPAGNE("샴페인 1개"),
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,10 +1,21 @@
 package christmas.view;
 
+import static christmas.view.OutputMessage.AMOUNT;
+import static christmas.view.OutputMessage.ORDER_MENU;
 import static christmas.view.OutputMessage.PREVIEW;
+
+import java.util.Map;
 
 public class OutputView {
 
     public void printPreview() {
         System.out.println(PREVIEW);
+    }
+
+    public void printOrderMenu(Map<String, Integer> orders) {
+        System.out.println(ORDER_MENU);
+        orders.entrySet().stream().forEach(order -> {
+            System.out.printf(AMOUNT.getMessage(), order.getKey(), order.getValue());
+        });
     }
 }
