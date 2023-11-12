e23eaeaf59c8b60eccb12491c1ea3d2dc98c3260
Haeun Jeong <gkdms4248@gmail.com>
2023-11-12 16:41:25 +0900

    test: 1-31 숫자 유효성 검사 테스트 수정

--- src/test/java/christmas/ValidatorTest.java
+++ src/test/java/christmas/ValidatorTest.java
@@ -1,28 +1,29 @@
 package christmas;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ValidatorTest {
-
-    @DisplayName("숫자인 경우 exception 을 반환하지 않는다.")
+    
+    @DisplayName("1-31 의 숫자인 경우 throw 가 발생하지 않는다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1", "2", "3", "40", "100"})
-    void validateDay(int day) {
+    @ValueSource(strings = {"1", "2", "3", "30", "31"})
+    void validateDay(String day) {
         // given
         Validator validator = new Validator();
 
         // when & then
-        assertDoesNotThrow(validator.validateDay(day));
+        assertDoesNotThrow(() -> validator.validateDay(day));
     }
 
-    @DisplayName("숫자가 아닌 경우 exception 을 반환한다.")
+    @DisplayName("1-31 의 숫자가 아닌 경우 throw 가 발생한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1.", ".2", "-3", "400", "2147483648"})
-    void validateNotDay(int day) {
+    @ValueSource(strings = {"1.", ".2", "ㄱ", "one", "32", "44"})
+    void validateNotDay(String day) {
         // given
         Validator validator = new Validator();
 
